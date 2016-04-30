bbb051df7f7080192543a8a5d00b7feb503d7529
Jairo Diaz <jairo.diaz@codescrum.com>
2016-04-30 13:46:34 +0100

    Added function

--- apps/cart/test/cart_test.exs
+++ apps/cart/test/cart_test.exs
@@ -2,7 +2,11 @@ defmodule CartTest do
   use ExUnit.Case
   doctest Cart
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "the cart should be empty" do
+    assert Cart.current == []
+  end
+
+  test "adding to cart should return ok" do
+    assert Cart.add("apple") == {:ok, ["apple"]}
   end
 end
