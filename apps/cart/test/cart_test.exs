6da1409dd0fb548f61dce8b92b37ce98e1ef4924
Thomas Walther <thomaswa@ccrma.stanford.edu>
2016-04-30 14:34:46 +0100

    add tests for adding items to cart

--- apps/cart/test/cart_test.exs
+++ apps/cart/test/cart_test.exs
@@ -2,21 +2,42 @@ defmodule CartTest do
   use ExUnit.Case
   doctest Cart
 
-  test "the cart should be empty" do
-    assert Cart.current == []
+  test "creating a cart creates an empty cart" do
+    assert Cart.new == %{current: [], events:[]}
   end
 
-  test "adding to cart should return ok" do
-    assert Cart.add("apple") == {:ok, _}
+  test "Cart.add should return ok" do
+    cart = Cart.new
+    assert Cart.add(cart, "apple") == {:ok, _}
   end
 
-  test "adding" do
-    %{
+  test "Cart.add should return a new cart with the item added to :current" do
+    cart = Cart.new
+    {:ok, cart} = Card.add(cart, "orange")
+    {:ok, cart} = Card.add(cart, "apple")
+
+    assert cart == %{
       current: ["apple", "orange"],
+      events: _
+    }
+  end
+
+  test "Cart.add should return a new cart with a new event prepended" do
+    cart = Cart.new
+    {:ok, cart} = Card.add(cart, "orange")
+    {:ok, cart} = Card.add(cart, "apple")
+
+    assert cart == %{
+      current: _,
       events: [
-        {"apple", :add, 2},
-        {"orange", :add, 1}
+        {"apple", :add, _},
+        {"orange", :add, _}
       ]
     }
+  end
+
+  @tag :skip
+  test "events should contain timestamps" do
+    
   end
 end
