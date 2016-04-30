d7a9723e10ae642e41a69b9803a29a2b8163e2c8
Dan Otto <theDanOtto@gmail.com>
2016-04-30 15:17:41 +0200

    determine data structure

--- apps/cart/test/cart_test.exs
+++ apps/cart/test/cart_test.exs
@@ -7,6 +7,16 @@ defmodule CartTest do
   end
 
   test "adding to cart should return ok" do
-    assert Cart.add("apple") == {:ok, ["apple"]}
+    assert Cart.add("apple") == {:ok, _}
+  end
+
+  test "adding" do
+    %{
+      current: ["apple", "orange"],
+      events: [
+        {"apple", :add, 2},
+        {"orange", :add, 1}
+      ]
+    }
   end
 end
